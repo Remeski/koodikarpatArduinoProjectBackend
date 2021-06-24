ce0f22cd37acafc23a2215d17a519a2b173906bf
Remeski <green.block909@gmail.com>
2021-06-24 14:00:56 +0300

    Reformatted data that GET /temperature returns

--- routes/Temperature.js
+++ routes/Temperature.js
@@ -3,11 +3,62 @@ const router = require('express').Router()
 const TempMeasurement = require('../Models/TempMeasurement')
 
 router.get('/', (req, res) => {
+  const accuracyInMilliseconds = 60000 // 1 minute
+
+  const getAllSensors = data => {
+    return data.reduce((acc, cur) => {
+      const { sensor } = cur
+
+      if (!acc.includes(sensor)) {
+        return [...acc].concat(sensor)
+      }
+      return acc
+    }, [])
+  }
+
+  const reformatData = data => {
+    const allSensorValues = data.map(elem => {
+      const { sensor, temperature, date } = elem
+
+      return { [sensor]: temperature, date }
+    })
+    const returnArray = allSensorValues.reduce((acc, cur) => {
+      if (acc.length < 1) {
+        return [...acc].concat(cur)
+      }
+
+      const index = acc.length - 1
+      const lastItem = { ...acc[index] }
+
+      if (!Object.keys(lastItem).includes(Object.keys(cur)[0])) {
+        const subtraction = cur.date.getTime() - lastItem.date.getTime()
+        if (subtraction <= accuracyInMilliseconds) {
+          const obj = {
+            [Object.keys(cur)[0]]: cur[Object.keys(cur)[0]],
+            ...lastItem,
+          }
+          return [...acc].slice(0, acc.length - 1).concat(obj)
+        }
+      }
+
+      return [...acc].concat(cur)
+    }, [])
+
+    return returnArray
+  }
+
   TempMeasurement.find({})
     .then(dbRes => {
-      res.json(dbRes)
+      const formattedData = reformatData(dbRes)
+      const allSensors = getAllSensors(dbRes)
+
+      const data = { allsensors: allSensors, data: [...formattedData] }
+      res.json(data)
     })
-    .catch(() => res.status(500).json({ error: 'Internal server error' }))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'Internal server error' })
+    })
 })
 
 // Protected
